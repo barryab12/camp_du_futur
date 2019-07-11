a1435c98f04e55a07ddfd89a45bcb7a3dd5504ed
aminata20 <nassongdiarrassouba75@gmail.com>
2019-07-11 18:16:48 +0000

    modification python et views

--- python.py
+++ python.py
@@ -1,8 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class NouveauModele(models.Model):
     _name = 'hr.employee'
     _inherit = 'hr.employee'
-    
-    nationalite = fields.Selection(string="nationalite",selection=[('Ivoirien'),('Etranger')])
-    probleme = fields.Many2one(comodel_name="purchase.order", string="achats")
+
+    salaire = fields.Integer(string= "votre salaire",required = True) 
+
+    multiplier = fields.Selection(string= "multiplier",selection=[('DOUBLER','doubler'),('TRIPLER','tripler')])
+
+    nouveau_salaire = fields.Integer(string= "nouveau_salaire",compute='_compute_nouveau_salaire',store = True) 
+    # compute empeche de saisir
+
+    @api.depends('salaire', 'multiplier')
+    def _compute_nouveau_salaire(self):
+        for r in self:
+            if  r.multiplier == 'DOUBLER':
+                r.nouveau_salaire = r.salaire * 2
+
+            elif r.multiplier == 'TRIPLER':
+                r.nouveau_salaire = r.salaire * 3
+
+            else:
+                r.nouveau_salaire = r.salaire
