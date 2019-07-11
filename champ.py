e98c7a48177bcef9e884c65608d4d73704840e3e
broucedrick <broucedrickatse@gmail.com>
2019-07-11 13:58:05 +0000

    Mise à jour vers les nouvelles tâches

--- champ.py
+++ champ.py
@@ -1,9 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class NouveauChamp(models.Model):
     _name = 'stock.picking'
     _inherit = 'stock.picking'
     # Creation du champ age
-    num = fields.Integer(string="Numéro", required=True)
-    genre = fields.Selection(string="Genre", selection=[('M','Masculin'),('F','Féminin')])
-    employ = fields.Many2one(comodel_name="hr.employee", string='Employé(e)')
+    salaire = fields.Integer(string="Votre salaire")
+    multiplier = fields.Selection(string="Multiplier", selection=[(2,'Doubler'),(3,'Tripler')])
+    # employ = fields.Many2one(comodel_name="hr.employee", string='Employé(e)')
+    nouveauSalaire =fields.Integer(string="Nouveau salaire", compute='multi', readonly=True)
+
+    @api.depends('salaire','multiplier')
+    def multi(self):
+    	for record in self:
+    		if record.multiplier == 2:
+    			record.nouveauSalaire = record.salaire * 2
+    		elif record.multiplier == 3:
+    			record.nouveauSalaire = record.salaire * 3
+    		else:
+    			record.nouveauSalaire = record.salaire
