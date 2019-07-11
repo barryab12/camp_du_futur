6841d24a8cc3a88a81a2239c1fc69c711b7e64e1
JoshuaGuei <52660092+JoshuaGuei@users.noreply.github.com>
2019-07-11 09:23:49 +0000

    jour2

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "tache1",
+    'summary': """ajout d'un nouveau champ dans un module""",
+    'description': """
+        Ce module permet d'ajouter le champ un type selection et  un champ de type many2one dans le module Employés
+    """,
+    'author': "Joshua Guei",
+    'depends': ['base'],
+    'data':["vue.xml"],
+    'website': "http://www.groupecerco.com"
+}
---
+++ pp.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class Tache(models.Model):
+    _name = 'hr.employee'
+    _inherit = 'hr.employee'
+
+    love = fields.Selection(string = "Aimez vous cette entreprise ?", selection=[('oui','OUI'),('non','NON')])
+    rapport = fields.Many2one(comodel_name='project.project', string="Projet Attribué")
