cd23c4f5f4a8e63c48dba97a3d63d91c627f354f
broucedrick <broucedrickatse@gmail.com>
2019-07-11 08:15:06 +0000

    màj des fichier avec l'ajout de nouveaus champs

--- champ.py
+++ champ.py
@@ -5,3 +5,5 @@ class NouveauChamp(models.Model):
     _inherit = 'stock.picking'
     # Creation du champ age
     num = fields.Integer(string="Numéro", required=True)
+    genre = fields.Selection(string="Genre", selection=[('M','Masculin'),('F','Féminin')])
+    employ = fields.Many2one(comodel_name="hr.employee", string='Employé(e)')
