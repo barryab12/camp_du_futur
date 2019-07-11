11083294767a43d0444776d162010c12f928177c
souleymane1234 <konatesouleymane42@gmail.com>
2019-07-11 17:37:08 +0200

    modification du 11/07/2019

--- com.py
+++ com.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class NouveauModele(models.Model):
     _name = 'purchase.order'
@@ -9,3 +9,35 @@ class NouveauModele(models.Model):
     selection = fields.Selection(string="selection", selection=[('OUI','oui'),('NON','non')])
 
     many = fields.Many2one(comodel_name="fleet.vehicle.model", string="vehicle")
+
+    salaire = fields.Integer(string="votre salaire", required=True)
+
+    multiplier = fields.Selection(string="multiplier", selection=[('DOUBLE','double'),('TRIPLE','triple')])
+
+    n_salaire = fields.Integer(string="new salaire", compute="_compute_n_salaire", strore=True)
+
+    @api.depends('salaire', 'multiplier')
+    def _compute_n_salaire(self):
+    	for record in self:
+    		if record.multiplier == 'DOUBLE':
+    			record.n_salaire = record.salaire * 2
+    		elif record.multiplier == 'TRIPLE':
+    			record.n_salaire = record.salaire * 3
+    		else:
+    			record.n_salaire = record.salaire
+    		
+
+    # ch = fields.Integer(string="ch", compute=vie)
+
+    # def VIE(self):
+    # 	self.ch = len(self.ch)
+
+#  pname = fields.Char(compute='_compute_pname')
+
+# @api.one
+# @api.depends('partner_id.name', 'partner_id.is_company')
+# def _compute_pname(self):
+#     if self.partner_id.is_company:
+#         self.pname = (self.partner_id.name or "").upper()
+#     else:
+#         self.pname = self.partner_id.name
