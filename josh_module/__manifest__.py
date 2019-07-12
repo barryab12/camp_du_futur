ebb80b2ac28f0d89e11701b52d8e0f87eb6c28fb
JoshuaGuei <52660092+JoshuaGuei@users.noreply.github.com>
2019-07-12 18:55:21 +0000

    Vendredi 12/07/19

---
+++ josh_module/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Extrait d'Acte de Naissance",
+    'version': '1.0.0.0',
+    'summary': """information sur le père et la mère""",
+    'description': """
+        Ce module permet d'enregistrer les infos suivantes:
+        - Nom
+        - Prénoms
+        - date de naissance
+        - ville de naissance
+        - profession
+        - photo
+    """,
+    'author': "Joshua Guei",
+    'images':[],
+    'license':'AGPL-3',
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+    'depends': ['base'],
+    'data':["views/vue.xml","security/ir.model.access.csv"],
+    'website': "http://www.groupecerco.com"
+}
