3c82860aaadcdf18565d3a029dec68d3b70c029f
aminata20 <nassongdiarrassouba75@gmail.com>
2019-07-25 09:01:37 +0000

    erreur de bool

---
+++ loi/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Evaluation de Notes",
+    'version': '1.0.0.0',
+    'summary': """Evaluer les notes """,
+    'category': 'Administration',
+
+    'description': """
+        Ce module permet d'evaluer les exercices d'autres personnes
+    
+    """,
+    'author': "Aminata",
+    'images':[],
+    'license':'AGPL-3',
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+    'depends': ['base'],
+    'data':["views/vue.xml","security/ir.model.access.csv"],
+    'website': "http://www.groupecerco.com"
+}
+
+
---
+++ loi/models/info.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields,api
+
+
+class EvaluationEvaluation(models.Model):
+	_name = 'evaluation.evaluation'
+	_rec_name = ("noter")
+	
+	noter = fields.Many2one('res.users',string='Selectionner')
+	note = fields.Selection(string=u'Donner une Note',selection=[('0', '0'), ('1', '1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10')])
+							
+    						
+	@api.onchange('note')
+	@api.multi
+	def _note(self):
+		i = 0
+		for rec in self:
+			for x in rec.note:
+				if x != '':
+					i += 1
+		rec.note = str(i)
+	
+
+	
+
+
+
+
+	
+	
+	
+	
