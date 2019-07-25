320949406d30aff717df65e7aa6b6febdf4739e8
souleymane1234 <konatesouleymane42@gmail.com>
2019-07-25 14:38:10 +0200

    ValueError: Wrong value for model.etudiant.note: 0

--- Devoir/models/models.py
+++ Devoir/models/models.py
@@ -13,18 +13,20 @@ class Etudiant(models.Model):
     # texte = fields.Char(default="Veuillez cliqueer sur le bouton ci-dessous pour déposer votre devoir", readonly=True)
     # mon_devoir = fields.One2many('model.depot', 'etudiant')
     # file_name = fields.Char(string="File Name")
-    nb_note = fields.One2many('model.depot', 'etudiant')
-    note = fields.Char(string="Note", compute='_note', readonly=True)
+    nb_note = fields.One2many('model.depot', 'student')
+    notes = fields.Char(string="Note", compute='_notes', readonly=True)
+    student = fields.Many2one('res.users', string="student")
+    note = fields.Selection(string="Note", default="0", selection=[('n0','0'),('n1','1'),('n2','2'),('n3','3'),('n4','4'),('n5','5'),('n6','6'),('n7','7'),('n8','8'),('n9','9'),('n10','10')], required=True, store=True)
 
     @api.onchange('nb_note')
     @api.multi
-    def _note(self):
+    def _notes(self):
         i = 0
         for rec in self:
             for n in rec.nb_note:
                 if n !='':
                     i += 1
-        rec.note = str(i)+" / 5"
+        rec.note = str(i)+" / 10"
     
 class Depot_devoir(models.TransientModel):
     _name = 'model.depot'
@@ -32,5 +34,4 @@ class Depot_devoir(models.TransientModel):
     # devoir = fields.Binary(string="Charger un fichier", store=True)
     # nom_fichier = fields.Char(string="Nom du fichier")
     student = fields.Many2one('res.users', string="student")
-    etudiant = fields.Many2one('model.etudiant', 'Votre nom')
     note = fields.Selection(string="Note", default="0", selection=[('n0','0'),('n1','1'),('n2','2'),('n3','3'),('n4','4'),('n5','5'),('n6','6'),('n7','7'),('n8','8'),('n9','9'),('n10','10')], required=True, store=True)
