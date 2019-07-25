4fb747cf53b4d057548b13f3d898feeeb6329e50
myriam125 <52348207+myriam125@users.noreply.github.com>
2019-07-25 11:23:16 +0100

    @api.onchange('note'): j'ai eu une erreur d'identation a ce niveau

--- codance/models/models.py
+++ codance/models/models.py
@@ -1,9 +1,21 @@
+
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
  
-class notation(models.Model):
+class Notation(models.Model):
     _name = 'model.notation'
-    _rec_name = ("candidat")
+    _rec_name = 'candidat'
 
-    note = fields.Char(string="Note", required=True)
     candidat  = fields.Many2one(commodel_name="res.users", string="candidat" , domain=[('model.notation','in',[5])])
+    note = fields.Selection(string='Note',selection=[('0', '0'), ('1', '1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10')])
+							
+    						
+	@api.onchange('note')
+	@api.multi
+	def _note(self):
+		i = 0
+		for rec in self:
+			for x in rec.note:
+				if x != '':
+					i += 1
+		rec.note = str(i)
