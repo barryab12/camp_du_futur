34a966df89b509b28c78ec77bc317d578575c846
myriam125 <52348207+myriam125@users.noreply.github.com>
2019-07-25 17:24:57 +0100

    mon module est installé mais on arrive pas a y acceder

--- codances/models/models.py
+++ codances/models/models.py
@@ -5,10 +5,10 @@ from odoo import models, fields,api
 
 class CandidatCandidat(models.Model):
 	_name = 'model.candidat'
-	_rec_name = ("noter")
+	_rec_name = "noter"
 	
 	noter = fields.Many2one('res.users',string='Selectionner')
-	note = fields.Selection(string=u'Noter',selection=[('0', '0'), ('1', '1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('1', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20')])
+	note = fields.Selection(string='Noter',selection=[('0', '0'), ('1', '1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10'),('11', '11'),('1', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20')])
 							
     						
 	@api.depends('note')
