b8c228d7c016db3e8079d4a01dcca5f8a419a925
Kevin <petrofall96@gmail.com>
2019-07-25 16:34:12 +0000

    creation d'une nouvelle classe pour differencier le candidat de l'admin

--- evaluation/models/main.py
+++ evaluation/models/main.py
@@ -5,9 +5,15 @@ from odoo import models, fields,api
 
 class EvalCandidat(models.Model):
 	_name = 'mod.candidat'
-	_rec_name = ("cdt")
+	# _rec_name = ("cdt")
 	
+	
+	
+
+
+class EvalAdmin(models.Model):
+	_name = 'mod.admin'
+
 	cdt = fields.Many2one('res.users', string='Candidats')
 	nom = fields.Many2one('res.users', string='Noté par')
 	note = fields.Char(string='Note', placeholder='Notez le candidat')
-	
