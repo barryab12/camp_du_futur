4b099f7c1cba9ba96e8aedeac76f77fc91601fa3
JoshuaGuei <52660092+JoshuaGuei@users.noreply.github.com>
2019-07-26 09:08:06 +0000

    KeyError: 'etudiant'

--- eval/__manifest__.py
+++ eval/__manifest__.py
@@ -14,17 +14,11 @@
     'author': "Joshua Guei",
     'website': "http://www.groupecerco.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
     ],
     'images':[],
--- eval/models/eval.py
+++ eval/models/eval.py
@@ -1,34 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from random import *
 
 class Candidat(models.Model):
     _name = 'candidat.candidat'
 
-    name = fields.Char(string="Nom", required=True)
+    name = fields.Char(string="Nom", required=True, store=True)
     prenom = fields.Char(string="Prénopm(s)", required=True)
     niveau = fields.Selection(string="Niveau", selection=[('L1','Licence 1'),('L2','Licence 2'),('L3','Licence 3')])
     filiere = fields.Selection(string="Filière", selection=[('SI','SEI / IIM'),('RT','RIT / TCOM'),('II','IDA / IG')])
-    # texte = fields.Char(default="Veuillez cliqueer sur le bouton ci-dessous pour déposer votre devoir", readonly=True)
-    # mon_devoir = fields.One2many('model.depot', 'etudiant')
-    # file_name = fields.Char(string="File Name")
-    nb_note = fields.One2many('depot.depot', 'etudiant')
-    note = fields.Char(string="Note", compute='_note', readonly=True)
+    note_chif = fields.One2many('model.depot', 'etudiant', store=True)
+    note = fields.Char(string="Note", compute='_note', readonly=True, store=True)
 
-    @api.onchange('nb_note')
+    identifiant = fields.Integer(string="Copie identifiant", default=lambda self: self._context.get('active_ids'), readonly=True, store=True)
+
+    @api.onchange('note_chif')
     @api.multi
     def _note(self):
         i = 0
         for rec in self:
-            if rec.nb_note.note != None:
-                i += 1
+            for n in rec.note_chif:
+                if n != '':
+                    i += 1
         rec.note = str(i)+" / 5"
 
-class Depot_devoir(models.TransientModel):
+    @api.multi
+    def _identifiant(self):
+        for c in self:
+            c.identifiant = c._context.get('active_id')
+
+class Depot(models.TransientModel):
     _name = 'depot.depot'
 
     # devoir = fields.Binary(string="Charger un fichier", store=True)
     # nom_fichier = fields.Char(string="Nom du fichier")
-    student = fields.Many2one('res.users', string="student")
-    etudiant = fields.Many2one('model.etudiant', 'Votre nom', required=True)
-    note = fields.Selection(string="Note", selection=[('n1','1'),('n2','2'),('n3','3'),('n4','4'),('n5','5')], required=True, store=True)
+    identifiant_can = fields.Char('Id name', default=lambda self: self.env['model.etudiant'])
+    candidat = fields.Many2one('candidat.candidat', 'Votre nom', domain=[('name','!=','identifiant_can')], required=True)
+    note = fields.Selection(string="Note", selection=[('A','0 - 5'),('B','5-10'),('C','10-15'),('D','15-20')], default="0", required=True, store=True)
